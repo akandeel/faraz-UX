4b6406003ceb17b0301dd96f258dc43cb824ffae
your github.com username <kandeel.a1@gmail.com>
2017-11-07 22:26:30 -0500

    before_cache to help turbolinks load calendar

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,6 +1,10 @@
-$(function () {
+$(document).ready(function () {
 
   $('#calendar').fullcalendar({
     events: '/events.json'
+  })
+
+  $(document).on('turbolinks:load', eventCalendar);
+  $(document).on('turbolinks:before-cache', clearCalendar)
 
   });
