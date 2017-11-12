3b687ca6f0e65178db886dc7f3d9acb9be105613
your github.com username <kandeel.a1@gmail.com>
2017-11-11 22:12:16 -0500

    add gem jquery, delete a useless route, create app-print.css.scss

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,10 +1,1 @@
-$(document).ready(function () {
 
-  $('#calendar').fullcalendar({
-    events: '/events.json'
-  })
-
-  $(document).on('turbolinks:load', eventCalendar);
-  $(document).on('turbolinks:before-cache', clearCalendar)
-
-  });
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get 'welcome' => 'pages#home'
 
+
+
 end
