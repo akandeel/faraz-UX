45f544027650b45031f5993c824b5158eac9ea71
your github.com username <kandeel.a1@gmail.com>
2017-11-14 21:08:10 -0500

    create content_tag to build html in ruby

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,1 +1,12 @@
 class Calendar < Struct.new(:view, :date, :callback)
+HEADER = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+START_DAY = :Sunday
+
+delegate :content_tag, to: :views
+
+
+def calendarTable
+  content_tag :calendarTable, class: "calendar_table table-bordered table-striped" do
+    header + weekRows
+  end
+end
